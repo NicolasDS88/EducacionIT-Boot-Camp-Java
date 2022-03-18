8085257f8c6683eaba889706a36389da55354083
Nicolas da Silva <39927474+NicolasDS88@users.noreply.github.com>
2022-03-18 14:34:16 -0300

    q
    q

--- circus_delSolei/src/circus_delSolei/JaulaAnimal.java
+++ circus_delSolei/src/circus_delSolei/JaulaAnimal.java
@@ -2,21 +2,18 @@ package circus_delSolei;
 
 public class JaulaAnimal {
 	
-	String nombreDueño;
-	Tigre[] jaulaTigres; //una jaula con solo tigres
-	Mono[] jaulaMonos;//una jaula con monos
-	abstractAnimal[] jaulaAnimales; //una jaula con animales mezclados?
-	public JaulaAnimal(String nombreDueño, abstractAnimal[] jaulaAnimales) {
-		super();
-		this.nombreDueño = nombreDueño;
-		this.jaulaAnimales = jaulaAnimales;
-	}
-
-	
+	abstractAnimal[] nombreDueño;
+	Mono[] monosComJaula; //adentro hay muchos monos
+	Tigre[] tigresConJaula;
 	
 	
 	
 }
+	
+	
+	
+
+
 
 
 
--- circus_delSolei/src/circus_delSolei/MainCircus.java
+++ circus_delSolei/src/circus_delSolei/MainCircus.java
@@ -4,11 +4,20 @@ public class MainCircus {
 
 	public static void main(String[] args) {
 		
+		Mono[] muchosMonos = new Mono[3];
+		JaulaAnimal[] jaulaAnimales=new JaulaAnimal[5]; //unMono.nombre() //unMono.suJaula()
 		
-		JaulaAnimal jaulaAnimales=new JaulaAnimal("Carlos", ); //unMono.nombre() //unMono.suJaula()
 		
 		
 		Mono unMono = new Mono("Carlos", 00000001l, "Banana");
+		Mono dosMono = new Mono("Juan", 00000002l, "Banana");
+		Mono tresMono = new Mono("NoSePuedeDecir", 00000003l, "Manzana");
+		
+		muchosMonos[0]=unMono;
+		muchosMonos[1]=dosMono;
+		muchosMonos[2]=tresMono;
+		
+		//jaulaAnimales[0]=muchosMonos; //como hago?
 		
 
 	}
