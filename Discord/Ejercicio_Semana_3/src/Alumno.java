f6eb12c0aaad376f391e059f217cfd0ad15a94f9
NicolasDS <nicolas.dasilva88@gmail.com>
2022-03-21 13:32:02 -0300

    ejercicio discord semana 3

--- Discord/Ejercicio_Semana_3/src/Alumno.java
+++ Discord/Ejercicio_Semana_3/src/Alumno.java
@@ -4,6 +4,13 @@ public class Alumno {
 	String apellido;
 	long idExamen;
 	byte nota; //del 0 al 10
+	public Alumno(String nombre, String apellido, long idExamen, byte nota) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.idExamen = idExamen;
+		this.nota = nota;
+	}
 	
 	
 	
--- Discord/Ejercicio_Semana_3/src/EscuelaXYZ.java
+++ Discord/Ejercicio_Semana_3/src/EscuelaXYZ.java
@@ -13,14 +13,14 @@ public class EscuelaXYZ {
 
 	// metodos
 
-	int pedirTamanioLista() {
+	int listaAlumnos() {
 		Scanner pidoNumero = new Scanner(System.in);
 		System.out.println("Ingrese la cantidad de alumnos pro favor: ");
 		int tamanio;
 		do {
 			System.out.println("Solo valores numericos:");
 			tamanio = pidoNumero.nextInt();
-		} while (tamanio <= 0);
+		} while (tamanio <=0);
 
 		return tamanio;
 
@@ -31,4 +31,29 @@ public class EscuelaXYZ {
 		listaAlumnos = listaActualizada; // apunta al nuevo espacio de memoria de la nueva lista
 	}
 
+	void agregarAlumnos(){
+		if (this.tamañoLista>0) {
+			System.out.println("Agregando alumnos en la lista...");//puedo agregar alumnos
+			Scanner pidoAlumno = new Scanner(System.in);
+			for (int i = 0; i < tamañoLista; i++) {
+				System.out.println("Ingrese NOMBRE del "+i+1+"º alumno");
+				String nombre=pidoAlumno.next();
+				System.out.println("Ingrese APELLIDO del "+i+1+"º alumno");
+				String apellido=pidoAlumno.next();
+				System.out.println("Ingrese ID del EXAMEN del "+i+1+"º alumno");
+				long idExamen=pidoAlumno.nextLong(); //pongo long por que nose como maneja los ID, quizas es muy grande
+				System.out.println("Ingrese la NOTA del "+i+1+"º alumno");
+				byte nota;
+				do{nota=pidoAlumno.nextByte();}
+				while (nota>=0&&nota<=10);// se que va del 0 al 10
+				
+				listaAlumnos[i]=new Alumno(nombre, apellido, idExamen, nota);
+			}
+			
+		} else {System.out.println("No se pueden agregar alumnos");}
+		
+	}
+	
+	
+	
 }
